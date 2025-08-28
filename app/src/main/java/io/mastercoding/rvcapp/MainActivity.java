00279b9ed43e8d71d10cd954e24879f6df5fcd21
Ngo Thanh Nam <Nam.NgoThanh@vn.bosch.com>
2025-08-28 14:25:16 +0700

    modify androidManifestfile -- add seconde layout -- dummy for CameraHal service call -- draft version

--- app/src/main/java/io/mastercoding/rvcapp/MainActivity.java
+++ app/src/main/java/io/mastercoding/rvcapp/MainActivity.java
@@ -2,23 +2,54 @@ package io.mastercoding.rvcapp;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Intents: facilitates communication bet. different components of an app,
+        //          as well as bet. different applications.
+
+        // types of intents:
+        // 1- Explicit Intents
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSecondActivity();
+            }
         });
+
+        // 2- Implicit Intents
+        Button btn2 = findViewById(R.id.openBrowser);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openEVSCameraHal();
+            }
+        });
+
     }
+    public void goToSecondActivity(){
+        Intent intent = new Intent(this, SecondActivity.class );
+        startActivity(intent);
+    }
+
+    public void openEVSCameraHal(){
+        Uri path = Uri.parse("");
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, path);
+
+        startActivity(intent);
+
+    }
+
 }
